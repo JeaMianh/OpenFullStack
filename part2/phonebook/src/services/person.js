82b750bacf5092ee7a9739b55d20b92913c4e76e
GoldenExperience <JeaMianh@foxmail.com>
2024-03-16 20:20:50 +0800

    修改URL 添加代理

--- part2/phonebook/src/services/person.js
+++ part2/phonebook/src/services/person.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getAll = async () => {
     const response = await axios.get(baseUrl)
